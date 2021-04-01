57558147ce7172cfd4bed9ea6a3f6dc5876e191c
OsservatorioSchiaparelli <51033665+OsservatorioSchiaparelli@users.noreply.github.com>
2021-04-01 21:21:25 +0200

    Remove update_container.sh in favor of docker-compose.yml

--- update_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker stop astrometrynet
-docker rm astrometrynet
-docker build --tag astrometrynet/astrometrynet:latest .
-#ASTROMETRY_INDEX_PATH='/media/nascdf/Platesolver/data'
-ASTROMETRY_INDEX_PATH='/home/osservatorio/astrometry.net/data-local'
-docker run --detach --net=host --volume=$ASTROMETRY_INDEX_PATH:/data1/INDEXES:Z --name=astrometrynet --restart=always astrometrynet/astrometrynet
-#docker logs -f astrometrynet
